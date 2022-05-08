fcca30edcc3135dda005241d3606851ed9d66678
Ced <jcedb07@gmail.com>
2022-05-09 00:11:29 +0800

    firebase firestore integration and loading indicator

--- _comps/CdxInput.tsx
+++ _comps/CdxInput.tsx
@@ -3,13 +3,22 @@ import styles from '../styles/_comps/input.module.scss';
 import { IBaseProps } from '../_core/models/base.model';
 
 interface Props extends IBaseProps {
+  value?: string | number;
   placeholder?: string;
+  onChange?: (value: string) => void;
 }
 
-function CdxInput({ placeholder }: Props) {
+function CdxInput({ value, placeholder, onChange }: Props) {
   return (
     <div className={styles['cdx-input']}>
-      <input type="text" placeholder={placeholder} />
+      <input
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={e => {
+          if (onChange) onChange(e.target.value);
+        }}
+      />
     </div>
   );
 }
--- _comps/CdxTextarea.tsx
+++ _comps/CdxTextarea.tsx
@@ -3,13 +3,21 @@ import styles from '../styles/_comps/textarea.module.scss';
 import { IBaseProps } from '../_core/models/base.model';
 
 interface Props extends IBaseProps {
+  value?: string;
   placeholder?: string;
+  onChange?: (value: string) => void;
 }
 
-function CdxTextarea({ placeholder }: Props) {
+function CdxTextarea({ value, placeholder, onChange }: Props) {
   return (
     <div className={styles['cdx-textarea']}>
-      <textarea placeholder={placeholder}></textarea>
+      <textarea
+        value={value}
+        placeholder={placeholder}
+        onChange={e => {
+          if (onChange) onChange(e.target.value);
+        }}
+      />
     </div>
   );
 }
